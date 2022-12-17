2aa7ec22cdd62baf693ba04718428432c1da63a1
shuonineshabby <shuonineshabby@qq.com>
2022-12-17 17:48:46 +0800

    renew

--- src/test.java
+++ src/test.java
@@ -1,9 +1,7 @@
-import java.util.*;
 public class test {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         String str = "l i k e,d";
-        String a[] = str.split(" ");//去除空格并在空格处进行分割
+        String[] a = str.split(" ");
         for (String s : a) System.out.println(s);
 
     }
